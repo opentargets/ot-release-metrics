26714cee7e3d49f962c3935e2f157ce3a9dc5fc5
ireneisdoomed <irene.lopezs@protonmail.com>
2021-05-17 11:10:49 +0100

    add_delta implementation

--- src/utils.py
+++ src/utils.py
@@ -7,16 +7,25 @@ def get_table_download_link_csv(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="ot_metrics_data.csv" target="_blank">Download data</a>'
     return href
 
+def add_delta(evidence, metric, previous_run, latest_run):
+    evidence[f"Δ in number of {metric}"] = (
+        evidence[f"Nr of {metric} in {latest_run.split('-')[0]}"]
+        .sub(
+            evidence[f"Nr of {metric} in {previous_run.split('-')[0]}"],
+            axis=0, fill_value=0
+        )
+    )
+
 def compare_evidence(
     evidence: pd.DataFrame,
     latest_run: str,
     previous_run: str
 ) -> pd.DataFrame:
-    evidence["Δ in number of evidence strings"] = evidence[f"Nr of evidence strings in {latest_run.split('-')[0]}"].sub(evidence[f"Nr of evidence strings in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
-    evidence["Δ in number of invalid evidence strings"] = evidence[f"Nr of invalid evidence strings in {latest_run.split('-')[0]}"].sub(evidence[f"Nr of invalid evidence strings in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
-    evidence["Δ in number of evidence strings dropped due to duplication"] = evidence[f"Nr of evidence strings dropped due to duplication in {latest_run.split('-')[0]}"].sub(evidence[f"Nr of evidence strings dropped due to duplication in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
-    evidence["Δ in number of evidence strings dropped due to unresolved target"] = evidence[f"Nr of evidence strings dropped due to unresolved target in {latest_run.split('-')[0]}"].sub(evidence[f"Nr of evidence strings dropped due to unresolved target in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
-    evidence["Δ in number of evidence strings dropped due to unresolved disease"] = evidence[f"Nr of evidence strings dropped due to unresolved disease in {latest_run.split('-')[0]}"].sub(evidence[f"Nr of evidence strings dropped due to unresolved disease in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
+    add_delta(evidence, "evidence strings", previous_run, latest_run)
+    add_delta(evidence, "invalid evidence strings", previous_run, latest_run)
+    add_delta(evidence, "evidence strings dropped due to duplication", previous_run, latest_run)
+    add_delta(evidence, "evidence strings dropped due to unresolved target", previous_run, latest_run)
+    add_delta(evidence, "evidence strings dropped due to unresolved disease", previous_run, latest_run)
     evidence = evidence.iloc[:-1]  # Delete row with NaN
     evidence.loc['Total'] = evidence.sum()
     return evidence[evidence.columns[-5:]]
@@ -26,8 +35,8 @@ def compare_association(
     latest_run: str,
     previous_run: str
 ) -> pd.DataFrame:
-    association["Δ in number of direct associations"] = association[f"Nr of direct associations in {latest_run.split('-')[0]}"].sub(association[f"Nr of direct associations in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
-    association["Δ in number of indirect associations"] = association[f"Nr of indirect associations in {latest_run.split('-')[0]}"].sub(association[f"Nr of indirect associations in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
+    add_delta(association, "direct associations", previous_run, latest_run)
+    add_delta(association, "indirect associations", previous_run, latest_run)
     association.loc['Total'] = association.sum()
     return association[association.columns[-2:]]
 
@@ -36,7 +45,7 @@ def compare_disease(
     latest_run: str,
     previous_run: str
 ) -> pd.DataFrame:
-    disease["Δ in number of diseases"] = disease[f"Nr of diseases in {latest_run.split('-')[0]}"].sub(disease[f"Nr of diseases in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
+    add_delta(disease, "diseases", previous_run, latest_run)
     return disease
 
 def compare_drug(
@@ -44,5 +53,5 @@ def compare_drug(
     latest_run: str,
     previous_run: str
 ) -> pd.DataFrame:
-    drug["Δ in number of drugs"] = drug[f"Nr of drugs in {latest_run.split('-')[0]}"].sub(drug[f"Nr of drugs in {previous_run.split('-')[0]}"], axis=0, fill_value=0)
+    add_delta(drug, "drugs", previous_run, latest_run)
     return drug
