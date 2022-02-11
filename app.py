8793b896e8cb2c9d11adb3a125f934ff1b6eff81
Irene López <irene.lopezs@protonmail.com>
2022-02-11 14:28:31 +0000

    Revert test

--- app.py
+++ app.py
@@ -202,7 +202,6 @@ if page == "Compare metrics":
             .rename({"value" : f"Nr of targets in {latest_run.split('-')[0]}"}, axis=1)
         )
 
-
         # Aggregate metrics
         evidence_datasets = [
             old_evidence_count, new_evidence_count,
