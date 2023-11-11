9137f9e498119a5e58392f4ac91926b6c770c9c9
Kirill Tsukanov <tsukanoffkirill@gmail.com>
2023-11-11 16:32:54 +0000

    refator: remove no longer need cluster init script

--- src/initialise_cluster.sh
+++
@@ -1,5 +0,0 @@
-python3 -m pip install -q configargparse strenum jsonpickle google-cloud-storage addict yapsy
-git clone -b master https://github.com/opentargets/platform-input-support
-cd platform-input-support
-python3 ./platform-input-support.py -steps Evidence -o $HOME/output &> $HOME/pis.log
-hadoop fs -copyFromLocal $HOME/output/prod/evidence-files /
